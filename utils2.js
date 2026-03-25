5fab74a02bba14b6c900010c7eaa069fcc558a72
infolapdholly-cell <infolapd.holly@gmail.com>
2026-03-25 19:45:12 +0100

    Update utils2.js

--- utils2.js
+++ utils2.js
@@ -191,7 +191,7 @@ export function buildSidebar(elementId, activePage, grade) {
     h += nav('amendes',      activePage, '◇', 'Amendes / PV',        'amendes.html');
     h += nav('rapports',     activePage, '▣', 'Rapports',            'rapports.html');
     h += nav('saisies',        activePage, '▨', 'Saisies',            'saisies.html');
-    h += nav('armes-civiles', activePage, '🔫', 'Armes civiles',    'armes-civiles.html');
+    h += nav('armes-civiles', activePage, '▤', 'Armes civiles',    'armes-civiles.html');
   }
 
   h += `<div class="sb-divider"></div><div class="sb-section">Personnel</div>`;
